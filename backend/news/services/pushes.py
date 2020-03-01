089d94ba5aea6ef698d50039e3b755bd29a6f3e7
Alim Lyumanov <alm.lyuman0v@gmail.com>
2020-03-01 17:58:40 +0400

    master, added checking exception at rabbit connection

--- backend/news/services/pushes.py
+++ backend/news/services/pushes.py
@@ -6,10 +6,14 @@ from protocol.python import news_pb2
 def msg_pushes(msg):
     _news = news_pb2.News()
     _news.title = msg['title']
-
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBITMQ_HOST))
-    channel = connection.channel()
-    channel.queue_declare(queue=settings.RABBITMQ_TOPIC)
-    channel.basic_publish(exchange='', routing_key=settings.RABBITMQ_TOPIC, body=_news.SerializeToString())
-    connection.close()
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=settings.RABBITMQ_HOST))
+        channel = connection.channel()
+        channel.queue_declare(queue=settings.RABBITMQ_TOPIC)
+        channel.basic_publish(exchange='', routing_key=settings.RABBITMQ_TOPIC, body=_news.SerializeToString())
+        connection.close()
+    except Exception as e:
+        # Падает по ошибке подключения, скорее всего из-за кривых настроек контейнера
+        print(e)
+        return
     return True
